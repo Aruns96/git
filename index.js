d496670f3fcf686e1053eddc28ea4142269b260c
Aruns96 <arun509577@gmail.com>
2023-08-11 11:41:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log(document.title);
+
+
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+document.all[10].textContent="hello";
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
+
+let headTitle = document.getElementById("header-title");
+console.log(headTitle);
+headTitle.textContent = "hello";
+headTitle.innerText = "world";
+headTitle.innerHTML = "<h3>hello hai</h3>";
+
+
+let header = document.getElementById("main-header");
+header.style.borderBottom = "solid 5px #000"; 
+
+let addItem = document.getElementById("add-item");
+addItem.style.color = "green";
+addItem.style.fontWeight = "bold";
+
