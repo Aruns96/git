f9b51d0dfc6632d18afbcf23b3be102cf3c2cf87
Aruns96 <arun509577@gmail.com>
2023-08-11 12:04:34 +0530

    second commit

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ let addItem = document.getElementById("add-item");
 addItem.style.color = "green";
 addItem.style.fontWeight = "bold";
 
+let items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
+
