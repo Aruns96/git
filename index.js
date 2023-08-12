f6e8c9bd005f9eebc02aca62a1f8b1d883a5ae41
Aruns96 <arun509577@gmail.com>
2023-08-12 23:54:26 +0530

    changes made

--- index.js
+++ index.js
@@ -1,126 +1,38 @@
-// console.log(document.title);
+let form = document.getElementById("addForm");
+let items = document.getElementById("items");
 
 
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// document.all[10].textContent="hello";
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let newItem = document.getElementById("item").value;
+
+    let li = document.createElement("li");
+    li.className="list-group-item";
+
+    li.appendChild(document.createTextNode(newItem));
 
 
-// let headTitle = document.getElementById("header-title");
-// console.log(headTitle);
-// headTitle.textContent = "hello";
-// headTitle.innerText = "world";
-// headTitle.innerHTML = "<h3>hello hai</h3>";
+    let delBtn = document.createElement("button");
+    delBtn.className = "btn btn-danger btn-sm float-right delete";
+    delBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(delBtn);
+
+    let editBtn = document.createElement("button");
+    editBtn.className = "btn btn-success btn-sm float-right edit";
+    editBtn.appendChild(document.createTextNode("edit"));
+    li.appendChild(editBtn);
+
+    
+    items.appendChild(li)
 
 
-// let header = document.getElementById("main-header");
-// header.style.borderBottom = "solid 5px #000"; 
+});
 
-// let addItem = document.getElementById("add-item");
-// addItem.style.color = "green";
-// addItem.style.fontWeight = "bold";
-
-// // let items = document.getElementsByClassName("list-group-item");
-// // items[2].style.backgroundColor = "green";
-
-// for(let i=0;i<items.length;i++){
-//     items[i].style.fontWeight="bold";
-// }
-
-// // let item5 = document.getElementsByClassName("item5");
-// // item5.style.backgroundColor = "red";
-
-// let item5 =  document.getElementsByTagName("li");
-// item5[4].style.backgroundColor = "red";
-
-// let input = document.querySelector('input[type="submit"]');
-// input.value = "GO";
-
-// let item2 = document.querySelector(".list-group-item:nth-child(2)");
-// item2.style.backgroundColor="green";
-// let item3 = document.querySelector(".list-group-item:nth-child(3)");
-// item3.style.display="none";
-
-
-// let item2qall = document.querySelectorAll(".list-group-item")[1];
-// item2qall.style.backgroundColor="green";
-
-// let odd = document.querySelectorAll("li:nth-child(odd)");
-
-// for(let i=0;i<odd.length;i++){
-//     odd[i].style.backgroundColor="green";
-// }
-
-
-//traversing the dom
-
-let  item = document.querySelector("#items");
-// console.log(item.parentNode);
-// item.parentNode.style.backgroundColor = "grey";
-// console.log(item.parentNode.parentNode);
-
-console.log(item.parentElement);
-item.parentElement.style.backgroundColor = "grey";
-console.log(item.parentElement.parentElement);
-
-console.log(item.childNodes);
-
-console.log(item.children);
-
-item.children[1].style.backgroundColor = "red";
-
-console.log(item.firstChild);
-
-console.log(item.firstElementChild);
-
-item.firstElementChild.textContent="hello first";
-console.log(item.lastChild);
-
-console.log(item.lastElementChild);
-
-item.lastElementChild.textContent="hello last";
-
-console.log(item.nextSibling);
-
-console.log(item.nextElementSibling);
-
-console.log(item.previousSibling);
-
-console.log(item.previousElementSibling);
-
-
-let newH = document.createElement("h1");
-newH.className = "hello";
-newH.id = "world";
-newH.setAttribute("title","hi");
-
-let newHText = document.createTextNode("Hello world!");
-newH.appendChild(newHText);
-
-
-let ul= document.querySelector("ul.list-group-item");
-let h2 = document.querySelector("h2.titleItems");
-
-h2.insertBefore(newH,ul);
-
-
-let newH3 = document.createElement("h3");
-newH3.className = "hello";
-newH3.id = "world";
-newH3.setAttribute("title","hi");
-
-let newH3Text = document.createTextNode("Hello world!");
-newH3.appendChild(newH3Text);
- 
-
-
-let li = document.querySelector(".list-group-item:nth-child(1)");
-
-
-
-li.parentNode.insertBefore(newH3,li);
+items.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete")){
+        if(confirm("Are you Sure?")){
+            let li = e.target.parentElement;
+            items.removeChild(li);
+        }
+    }
+});
