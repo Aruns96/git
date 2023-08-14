89e7e0e7ed124306782953477a2257e45f7e5939
Aruns96 <arun509577@gmail.com>
2023-08-14 12:33:29 +0530

    edit button done

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ form.addEventListener("submit",(e)=>{
     btn.setAttribute("class","delete");
     btn.appendChild(document.createTextNode("Delete"));
     li.appendChild(btn);
+    
+    const editBtn = document.createElement("input");
+    editBtn.setAttribute("type","button");
+    editBtn.setAttribute("value","edit");
+    editBtn.setAttribute("class","edit");
+    editBtn.appendChild(document.createTextNode("Edit"));
+    li.appendChild(editBtn);
+
     users.appendChild(li);
     let obj = {
         username : nam.value,
@@ -35,6 +43,14 @@ users.addEventListener("click",(e)=>{
         let li = e.target.parentElement;
         users.removeChild(li);
     }
+    if(e.target.classList.contains("edit")){
+        let li = e.target.parentElement;
+        users.removeChild(li);
+        nam.value = Array.from(e.target.parentElement.textContent).join("").split(",")[0];
+        email.value = Array.from(e.target.parentElement.textContent).join("").split(",")[1];
+        phone.value = Array.from(e.target.parentElement.textContent).join("").split(",")[2].substring(0,Array.from(e.target.parentElement.textContent).join("").split(",")[2].length-10);
+    }
+
     let mail = Array.from(e.target.parentElement.textContent).join("").split(",")[1];
     localStorage.removeItem(mail);
 });
