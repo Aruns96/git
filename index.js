6b0a59deb4217e569511c7e45fbfd54223f9bf14
Aruns96 <arun509577@gmail.com>
2023-08-14 12:04:49 +0530

    delete data from local

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ form.addEventListener("submit",(e)=>{
     e.preventDefault();
     const li = document.createElement("li");
     li.appendChild(document.createTextNode(`${nam.value} ,${email.value},${phone.value}`));
+    const btn = document.createElement("input");
+    btn.setAttribute("type","button");
+    btn.setAttribute("value","delete");
+    btn.setAttribute("class","delete");
+    btn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(btn);
     users.appendChild(li);
     let obj = {
         username : nam.value,
@@ -18,8 +24,18 @@ form.addEventListener("submit",(e)=>{
 
     let objS = JSON.stringify(obj);
     localStorage.setItem(email.value,objS);
-    
+
     nam.value = "";
     email.value = "";
     phone.value = "";
 });
+
+users.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete")){
+        let li = e.target.parentElement;
+        users.removeChild(li);
+    }
+    let mail = Array.from(e.target.parentElement.textContent).join("").split(",")[1];
+    localStorage.removeItem(mail);
+});
+
