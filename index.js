d4498816df8d465999fec4f7d7179142ea1ca15c
Aruns96 <arun509577@gmail.com>
2023-08-14 11:27:34 +0530

    more users added

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const form = document.querySelector("#my-form");
 const nam = document.querySelector("#name");
 const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const users = document.querySelector("#users");
 
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${nam.value} ,${email.value},${phone.value}`));
+    users.appendChild(li);
     let obj = {
         username : nam.value,
-        useremail : email.value
+        useremail : email.value,
+        userphone : phone.value
     }
 
     let objS = JSON.stringify(obj);
-    localStorage.setItem("userdetails",objS);
-    
+    localStorage.setItem(email.value,objS);
+    nam.value = "";
+    email.value = "";
+    phone.value = "";
 });
