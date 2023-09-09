9c00899379726c417e6c724854ee22caa52e7899
Aruns96 <arun509577@gmail.com>
2023-09-09 14:42:22 +0530

    promise 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createPost(post){
              posts.push({...post,lastactivitytime : new Date().getTime()});
              const err = false;
              if(!err){
-                resolve();
+                resolve("posted");
              }else{
                 reject("error:something went wrong");
              }
@@ -59,10 +59,68 @@ function updateLastUserActivityTime(){
     })
 }
 
-Promise.all([createPost({title: 'Post Five', body: 'This is Post Five'}), updateLastUserActivityTime()])
-.then(getPosts)
-.catch(err => console.log(err))
-.then(deletePost)
-.catch(err => console.log(err))
+// Promise.all([createPost({title: 'Post Five', body: 'This is Post Five'}), updateLastUserActivityTime()])
+// .then(getPosts)
+// .catch(err => console.log(err))
+// .then(deletePost)
+// .catch(err => console.log(err))
+
+
+async function showItmes(){
+    let m1 = await createPost({title: 'Post Five', body: 'This is Post Five'});
+    console.log(m1);
+    getPosts();
+    let m2 = await deletePost();
+    console.log(m2)
+}
+showItmes();
+
+//tickets booking
+
+async function bookMovie(){
+const wifebringtickets = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("tickets")
+    },3000)
+})
+
+let ticket = await wifebringtickets;
+console.log(`i have ${ticket}`)
+
+const getpopcorn = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("popcorn")
+    },3000)
+})
+
+let popcorn = await getpopcorn;
+console.log(`get me some  ${popcorn}`)
+
+const getbutter = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("butter")
+    },3000)
+})
+
+let butter = await getbutter;
+console.log(`get  some  ${butter} on top`)
+
+const getcolddrinks = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("cold drink")
+    },3000)
+})
+
+let cooldrink = await getcolddrinks;
+console.log(`get  some  ${cooldrink} also`)
+
+
+return ticket;
+
+
+
+
+}
+bookMovie().then((m)=>{console.log(m)})
 
 
