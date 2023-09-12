7c3ec2405a92b9bf456ea72a3d2b5a75049b0e4c
Aruns96 <arun509577@gmail.com>
2023-09-12 10:40:01 +0530

    POST

---
+++ main1.js
@@ -0,0 +1,58 @@
+const form = document.querySelector("#my-form");
+const nam = document.querySelector("#name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const users = document.querySelector("#users");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${nam.value} ,${email.value},${phone.value}`));
+    const btn = document.createElement("input");
+    btn.setAttribute("type","button");
+    btn.setAttribute("value","delete");
+    btn.setAttribute("class","delete");
+    btn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(btn);
+
+    const editBtn = document.createElement("input");
+    editBtn.setAttribute("type","button");
+    editBtn.setAttribute("value","edit");
+    editBtn.setAttribute("class","edit");
+    editBtn.appendChild(document.createTextNode("Edit"));
+    li.appendChild(editBtn);
+
+    users.appendChild(li);
+    let obj = {
+        username : nam.value,
+        useremail : email.value,
+        userphone : phone.value
+    }
+    axios.post("https://crudcrud.com/api/969b6893e4ef4135950cfb0d6e8c80d6/appData" ,{obj})
+    .then(res =>{
+        console.log(res)
+    })
+    .catch(e => console.log(e))
+
+    // let objS = JSON.stringify(obj);
+    // localStorage.setItem(email.value,objS);
+    nam.value = "";
+    email.value = "";
+    phone.value = "";
+});
+users.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete")){
+        let li = e.target.parentElement;
+        users.removeChild(li);
+    }
+    if(e.target.classList.contains("edit")){
+        let li = e.target.parentElement;
+        users.removeChild(li);
+       
+        nam.value = Array.from(e.target.parentElement.textContent).join("").split(",")[0];
+        email.value = Array.from(e.target.parentElement.textContent).join("").split(",")[1];
+        phone.value = Array.from(e.target.parentElement.textContent).join("").split(",")[2].substring(0,Array.from(e.target.parentElement.textContent).join("").split(",")[2].length-10);
+    }
+
+    let mail = Array.from(e.target.parentElement.textContent).join("").split(",")[1];
+    localStorage.removeItem(mail);
+});
